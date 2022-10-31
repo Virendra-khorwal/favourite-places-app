8803c0af93632b62ae19732ce401ca8f9e7cf16e
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2022-10-31 14:43:04 +0530

    place item add

---
+++ components/Places/PlaceItem.jsx
@@ -0,0 +1,25 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const PlaceItem = ({place, onSelect}) => {
+  return (
+    <Pressable onPress={onSelect} >
+      <View>
+        <Image source={{uri: place.imageUri}} />
+        <View>
+          <Text>
+            {place.title}
+          </Text>
+          <Text>
+            {place.address}
+          </Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({
+    
+})
---
+++ screens/AllPlaces.jsx
@@ -0,0 +1,7 @@
+import PlaceList from "../components/Places/PlacesList"
+
+const AllPlaces = () => {
+    return <PlaceList />
+}
+
+export default AllPlaces;
