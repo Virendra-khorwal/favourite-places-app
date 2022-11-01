5a2dd21bbe2b1dbb66317a4c9370f7fae7037bc3
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2022-11-01 17:47:54 +0530

    navigation add

---
+++ components/Places/PlaceForm.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+
+const PlaceForm = () => {
+    return <View>
+        <Text>
+            The Place Form 
+        </Text>
+    </View>
+}
+
+export default PlaceForm;
